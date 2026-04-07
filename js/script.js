5814a70c186492041f7dd51ec56ee68bb36ddcf4
annajuliacordeiro <anna.7556622@aluno.mg.gov.br>
2026-04-07 18:14:57 +0000

    yyuyuu

--- js/script.js
+++ js/script.js
@@ -1,31 +1,36 @@
 let likeCount = 0;
-let curtido= false; // flag booleano
+let desCount = 0;
+
+let curtido = false; // flag booleana
+let descurtido = false;
 
 function curtir() {
 
  if(curtido == false){
-   likeCount++;
-   curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-}else{
+    likeCount++;
+    curtido = true;
+    document.getElementById("likeCount").innerText = likeCount;
+ }else{
     likeCount--;
-   curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
-}
-}
-
-function descurtir() {
-
- if(curtido == false){
-   dislikeCount++;
-   descurtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-
-}else{
-    dislikeCount--;
-   descurtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
-}
+    curtido = false;
+    document.getElementById("likeCount").innerText = likeCount;
  }
+}
+
+
+
+ function deslike () {
+if(descurtido == false){
+    desCount++;
+    descurtido = true;
+    document.getElementById("desCount").innerText = desCount;
+ }else{
+    desCount--;
+    descurtido = false;
+    document.getElementById("desCount").innerText = desCount;
+ }
+  
+}
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("desBtn").addEventListener("click", deslike);
